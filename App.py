02d36d08205f6ca520d0d524ec975166780ca9cb
Yussi E <37580970+yussieik@users.noreply.github.com>
2020-03-04 15:53:55 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,35 +1,37 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Mar  2 15:52:12 2020
-
 @author: YUSS
 """
 import pathlib
 import csv
+import pandas as pd
 from math import sin, cos, sqrt, atan2, radians
+import sys
 import os
 from werkzeug import secure_filename
 from flask import Flask, jsonify, request, render_template, flash, redirect, url_for, send_from_directory
+from flask_restful import Resource
 import webbrowser
-
+import json
 
 current_dir = pathlib.Path.cwd()
 
 app = Flask(__name__, template_folder = 'templates')
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = current_dir / 'uploads'
-app.config['DOWNLOAD_FOLDER'] = current_dir / 'downloads'
-ALLOWED_EXTENSIONS = {'csv'}
 
+app.secret_key = "secret key"
+app.config['WORK_FOLDER'] = current_dir / 'work'
+
+ALLOWED_EXTENSIONS = {'csv'}
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-	
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+ 
 @app.route('/')
 def upload_form():
-	return render_template('file_upload_form.html')
+    return render_template('file_upload_form.html')
   
 @app.route('/', methods=['GET', 'POST'])
-def get_address():
+def getAddress():
    if request.method == 'POST':
        if 'file' not in request.files:
            print('No file attached in request')
@@ -40,18 +42,18 @@ def get_address():
            return redirect(request.url)
        if file and allowed_file(file.filename):
            filename = secure_filename(file.filename)
-           file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-           process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+           file.save(os.path.join(app.config['WORK_FOLDER'], filename))
+           process_file(os.path.join(app.config['WORK_FOLDER'], filename))
            return redirect(url_for('uploaded_file', filename=filename))
    return render_template('file_upload_form.html')
       
-def process_file(points):
+def process_file(filename):
     
-    def upload_file():
-        if request.method == 'POST':
-          points = request.files['file']
-          points.save(secure_filename(points.filename))
-          return points
+##    def upload_file():
+##        if request.method == 'POST':
+##          points = request.files['file']
+##          points.save(secure_filename(points.filename))
+##          return points
     
     def calculate_distance(lat1, lon1, lat2, lon2):
         # approximate radius of earth in mm
@@ -64,7 +66,7 @@ def process_file(points):
         d = radius * c
         return d    
         
-    with open(points, newline='') as csvfile:
+    with open(filename, newline='') as csvfile:
       reader = csv.DictReader(csvfile)
       points = []
       links = []
@@ -85,18 +87,15 @@ def process_file(points):
         else:
             for j in range(i+2, len(l), 2):
                     links.append({'name': l[i] + l[j], 'distance': calculate_distance(float(l[i+1][0]), float(l[i+1][1]), float(l[j+1][0]), float(l[j+1][1]))})
+    
+    
     jsonf = {"Points" : points, "Links" : links}
-    return jsonify(jsonf)    
-        
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-   return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
 
-                        
+    with open(filename, 'w', encoding = "utf-8") as f:
+        json.dump(jsonf, f, ensure_ascii=False, indent=4)            
+        
+
 if __name__ == "__main__":
     webbrowser.open('http://localhost:5000')
     port = int(os.environ.get("PORT", 5000))
     app.run(host='127.0.0.1', port=port)
-            
-            
-       
